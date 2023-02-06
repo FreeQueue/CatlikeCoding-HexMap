9730b7f48321c70fde4f3d3665969233f47a8124
孔维焜 <1813508035@qq.com>
2023-02-07 00:42:17 +0800

    3.Elevation

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -5,13 +5,40 @@ public class HexCell : MonoBehaviour
 	#region Serialized Fields
 	public HexCoordinates coordinates;
 	public Color color;
+	public RectTransform uiRect;
 	[SerializeField] private HexCell[] neighbors;
 	#endregion
+
+	private int elevation;
+	public int Elevation {
+		get => elevation;
+		set {
+			elevation = value;
+			Vector3 position = transform.localPosition;
+			position.y = value * HexMetrics.elevationStep;
+			transform.localPosition = position;
+
+			Vector3 uiPosition = uiRect.localPosition;
+			uiPosition.z = elevation * -HexMetrics.elevationStep;
+			uiRect.localPosition = uiPosition;
+		}
+	}
 
 	public HexCell GetNeighbor(HexDirection direction) => neighbors[(int)direction];
 
 	public void SetNeighbor(HexDirection direction, HexCell cell) {
 		neighbors[(int)direction] = cell;
 		cell.neighbors[(int)direction.Opposite()] = this;
+	}
+	
+	public HexEdgeType GetEdgeType (HexDirection direction) {
+		return HexMetrics.GetEdgeType(
+			elevation, neighbors[(int)direction].elevation
+		);
+	}
+	public HexEdgeType GetEdgeType (HexCell otherCell) {
+		return HexMetrics.GetEdgeType(
+			elevation, otherCell.elevation
+		);
 	}
 }
--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -11,6 +11,8 @@ public class HexMapEditor : MonoBehaviour
 
 	private Color activeColor;
 
+	private int activeElevation;
+
 	#region Event Functions
 	private void Awake() {
 		SelectColor(0);
@@ -23,13 +25,24 @@ public class HexMapEditor : MonoBehaviour
 	}
 	#endregion
 
+	
 	private void HandleInput() {
 		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
-		if (Physics.Raycast(inputRay, out hit)) hexGrid.ColorCell(hit.point, activeColor);
+		if (Physics.Raycast(inputRay, out hit)) EditCell(hexGrid.GetCell(hit.point));
+	}
+
+	private void EditCell(HexCell cell) {
+		cell.color = activeColor;
+		cell.Elevation = activeElevation;
+		hexGrid.Refresh();
 	}
 
 	public void SelectColor(int index) {
 		activeColor = colors[index];
+	}
+
+	public void SetElevation(float elevation) {
+		activeElevation = (int)elevation;
 	}
 }
